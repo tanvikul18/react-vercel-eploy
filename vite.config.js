1569af1917f1b50ed7f39b46d6d1d3f72b6e3c2d
tanvi kulkarni <tanvi18.kulkarni@gmail.com>
2025-05-30 23:00:11 +0530

    third commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BAE_PATH || "/react-vercel-eploy"
+  base: process.env.VITE_BASE_PATH || "/react-vercel-eploy"
 })
